98b2ac8e09ef16e564e714ca42f6b36ff6a4a836
JETSADA UDOMDIREKRIRK (CDGS-ADD) <jetsada.u@cdg.co.th>
2021-10-01 09:42:49 +0700

    case01 for RegisterException

---
+++ src/test/java/th/cd/cdgs/ttd_day01/RegisterBusinessTest.java
@@ -0,0 +1,23 @@
+package th.cd.cdgs.ttd_day01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import badcode.ArgumentNullException;
+import badcode.RegisterBusiness;
+import badcode.Speaker;
+
+class RegisterBusinessTest {
+
+	@Test
+	public void case01() {
+		RegisterBusiness rb = new RegisterBusiness();
+		Exception e = assertThrows(ArgumentNullException.class, ()->{
+			rb.register(null, new Speaker());
+			
+		});
+		assertEquals("First name is required.", e.getMessage());
+	}
+
+}
