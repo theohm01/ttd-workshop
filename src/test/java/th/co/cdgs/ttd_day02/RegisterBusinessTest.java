e448858e3a00df86b8abfdd2c46e4453b2f18f34
JETSADA UDOMDIREKRIRK (CDGS-ADD) <jetsada.u@cdg.co.th>
2021-10-01 09:52:44 +0700

    add case01 with blank

--- src/test/java/th/co/cdgs/ttd_day02/RegisterBusinessTest.java
+++ src/test/java/th/co/cdgs/ttd_day02/RegisterBusinessTest.java
@@ -2,6 +2,7 @@ package th.co.cdgs.ttd_day02;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import badcode.ArgumentNullException;
@@ -11,6 +12,7 @@ import badcode.Speaker;
 class RegisterBusinessTest {
 
 	@Test
+	@DisplayName("case First Name require null")
 	public void case01() {
 		RegisterBusiness rb = new RegisterBusiness();
 		Exception e = assertThrows(ArgumentNullException.class, ()->{
@@ -18,6 +20,33 @@ class RegisterBusinessTest {
 			
 		});
 		assertEquals("First name is required.", e.getMessage());
+	}
+	
+	@Test
+	@DisplayName("case First Name require Blank")
+	public void case01WithBlank() {
+		RegisterBusiness rb = new RegisterBusiness();
+		Exception e = assertThrows(ArgumentNullException.class, ()->{
+			Speaker s = new Speaker();
+			s.setFirstName("");
+			rb.register(null, s);
+			
+		});
+		assertEquals("First name is required.", e.getMessage());
+	}
+	
+	@Test
+	@DisplayName("case Last Name require")
+	public void case02() {
+		RegisterBusiness rb = new RegisterBusiness();
+		Exception e = assertThrows(ArgumentNullException.class, ()->{
+			Speaker s = new Speaker();
+			s.setFirstName("test");
+			rb.register(null, s);
+			
+		});
+		assertEquals("Last name is required.", e.getMessage());
+		
 	}
 
 }
